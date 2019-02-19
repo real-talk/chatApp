3287b0ec3c344482be78de9638e48b1fbab91ba9
Marcus Aurelius <markmayfield16@gmail.com>
2019-02-19 13:46:25 -0600

    fakeDB setup, a few errors, circle back

---
+++ server/entity/message.ts
@@ -0,0 +1,19 @@
+import Recipient from './recipient'
+
+export enum MessageType {
+  PICTURE,
+  TEXT,
+  LOCATION,
+}
+
+export interface Message {
+  id: string
+  chatId: string
+  senderId: string
+  content: string
+  createdAt: number
+  type: MessageType
+  recipients: Recipient[]
+  holderIds: string[]
+}
+export default Message
