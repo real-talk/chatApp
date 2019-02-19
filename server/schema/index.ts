26b0db37ba90e1491aeb7bbc71d2833302dad014
Marcus Aurelius <markmayfield16@gmail.com>
2019-02-19 12:05:23 -0600

    server up and running

---
+++ server/schema/index.ts
@@ -0,0 +1,7 @@
+import { makeExecutableSchema } from 'apollo-server-express'
+import resolvers from './resolvers'
+import typeDefs from './typeDefs'
+export default makeExecutableSchema({
+  typeDefs,
+  resolvers,
+})
---
+++ server/schema/typeDefs.ts
@@ -0,0 +1,8 @@
+export default `
+  type Chat {
+    id: ID!
+  }
+  type Query {
+    chats: [Chat!]!
+  }
+`
